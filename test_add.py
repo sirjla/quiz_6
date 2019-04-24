e9a48ed60e322ecf3e46444a98115835477a9ffe
sirjla <joseluis@assadi.io>
2019-04-23 23:18:58 -0400

    test: add two coma separated digits

--- test_add.py
+++ test_add.py
@@ -5,3 +5,6 @@ from add import Add
 class TestAdd(unittest.TestCase):
 	def test_adds_single_number(self):
 		self.assertEqual(Add('1'), 1)
+
+	def test_adds_two_coma_separated_digits(self):
+		self.assertEqual(Add('1,2'), 3)
