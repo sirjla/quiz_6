c1d57d5085c6e19b94a09c74b04f2912a62db6a5
sirjla <joseluis@assadi.io>
2019-04-23 23:27:26 -0400

    refactor

--- add.py
+++ add.py
@@ -1,8 +1,3 @@
 def Add(text):
 	numbers = text.split(',')
-	if len(numbers) == 1:
-		return int(numbers[0])
-	if len(numbers) == 2:
-		return int(numbers[0]) + int(numbers[1])
-	else:
-		return int(numbers[0]) + int(numbers[1]) + int(numbers[2])
+	return sum(map(int, numbers))
