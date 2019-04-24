5c793005810a53e23af4a9f0ec58ac2670927510
sirjla <joseluis@assadi.io>
2019-04-23 23:45:29 -0400

    refactor

--- add.py
+++ add.py
@@ -1,6 +1,5 @@
 def Add(text, symbol=','):
-	numbers = text.split(symbol)
-	numbers = filter(lambda number: str.isdigit(number), numbers)
+	numbers = filter(lambda number: str.isdigit(number), text.split(symbol))
 	numbers = map(int, numbers)
 	numbers = filter(lambda number: number>0, numbers)
 	return sum(numbers)
