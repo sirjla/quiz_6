35c646a985d79b1082add93d7fd2069d8dad85cd
sirjla <joseluis@assadi.io>
2019-04-23 23:38:51 -0400

    code: non number in text

--- add.py
+++ add.py
@@ -1,6 +1,7 @@
 def Add(text):
 	numbers = text.split(',')
 	numbers = filter(lambda number: number != '', numbers)
+	numbers = filter(lambda number: str.isdigit(number), numbers)
 	numbers = map(int, numbers)
 	numbers = filter(lambda number: number>0, numbers)
 	return sum(numbers)
