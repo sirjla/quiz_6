e9adcf0106912071a16f8fe9e455caa514f18dd8
sirjla <joseluis@assadi.io>
2019-04-23 23:23:52 -0400

    code: add 3 coma separated numbers

--- add.py
+++ add.py
@@ -2,4 +2,7 @@ def Add(text):
 	numbers = text.split(',')
 	if len(numbers) == 1:
 		return int(numbers[0])
-	return int(numbers[0]) + int(numbers[1])
+	if len(numbers) == 2:
+		return int(numbers[0]) + int(numbers[1])
+	else:
+		return int(numbers[0]) + int(numbers[1]) + int(numbers[2])
